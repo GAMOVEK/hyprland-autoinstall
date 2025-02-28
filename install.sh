f830dcd6fa6b4aca83cd1c2446e7890416907279
GAMOVEK <80977792+GAMOVEK@users.noreply.github.com>
2025-02-28 23:39:29 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+sudo pacman -Syu
+sudo pacman -S base-devel linux-headers
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+Sudo pacman -S hyprland kitty gtk3 rofi-wayland dolphin sddm sddm-kcm
+echo wrappedhl > ~/wrappedhl
+echo .bash_profile > ~/.bash_profile
+echo 00-hyprland.conf > /etc/sddm.conf.d/00-hyprland.conf
