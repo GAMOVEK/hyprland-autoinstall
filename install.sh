8a4450d387d35931ae9e49604ec16d0d2860beea
GAMOVEK <80977792+GAMOVEK@users.noreply.github.com>
2025-03-01 00:02:21 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sudo pacman -Syu
-sudo pacman -S base-devel linux-headers -y
-Sudo pacman -S hyprland kitty gtk3 rofi-wayland dolphin sddm sddm-kcm git sudo -y
+sudo pacman -S base-devel linux-headers --noconfirm
+sudo pacman -S hyprland kitty gtk3 rofi-wayland dolphin sddm sddm-kcm git sudo --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
-makepkg -si
+makepkg -si --noconfirm
 echo wrappedhl > ~/wrappedhl
 echo .bash_profile > ~/.bash_profile
 echo 00-hyprland.conf > /etc/sddm.conf.d/00-hyprland.conf
