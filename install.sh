48a18d91c53a3f2ff57a705a1da1d4f5307793ee
GAMOVEK <80977792+GAMOVEK@users.noreply.github.com>
2025-03-15 20:19:07 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo pacman -Syyu
 sudo pacman -S base-devel linux-headers --noconfirm
-sudo pacman -S hyprland kitty gtk3 rofi-wayland dolphin sddm sddm-kcm git nano wget --noconfirm
+sudo pacman -S hyprland kitty gtk3 rofi-wayland dolphin sddm sddm-kcm git nano wget cron --noconfirm
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
@@ -9,11 +9,7 @@ sudo yay -Syyu
 cd ..
 
 sudo systemctl enable sddm
-cat hypr.sh | sudo tee /opt/hypr.sh
-sudo chmod +x /opt/mon_script.sh
-cat hypr.service | sudo tee /etc/systemd/system/hypr.service
-sudo systemctl daemon-reload
-sudo systemctl enable hypr.service
+
 #tlp batterie
 pacman -S tlp tlp-rdw --noconfirm
 systemctl enable tlp.service
